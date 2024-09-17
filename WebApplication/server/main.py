af3e11384dd7cb576905a189c905696c3d41152c
Eliyah Paslovsky <eliyahpaz101@gmail.com>
2024-09-17 14:46:56 +0200

    Backend Data Preprocessing

--- WebApplication/server/main.py
+++ WebApplication/server/main.py
@@ -1,23 +1,55 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import pandas as pd
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
 
+def map_ratings(df, column_name):
+    # Define the mapping dictionary
+    health_mapping = {
+        'Excellent': 1,
+        'Very Good': 2,
+        'Good': 3,
+        'Fair': 4,
+        'Poor': 5
+    }
+    
+    # Apply the mapping to the specified column
+    if column_name in df.columns:
+        df[column_name] = df[column_name].map(health_mapping)
+    else:
+        print(f"Column {column_name} not found in DataFrame")
+    
+    return df
 
-@app.route('/api/users', methods=['GET'])
-def users():
-    return jsonify({'users': ['Zach', 'Jess']})
+def preprocess_data(input_data):
+    #Define column names
+    column_names = [
+        'Age', 'Height', 'Weight', 'Waist', 'Male',
+        'GenHealth', 'OvrDietHealth', 'EducationLvl', 'EverSmoked',
+        'AgeStartSmoking', 'Smoker', 'CigsPerDay', 'AgeQuitSmoking', 'AlcConsump',
+        'ALcConsumpAmtPerDrinkDay', 'Diabetes', 'Cholesterol', 'Stroke', 'ModActivity', 
+    ]
+    # Convert input data to DataFrame
+    print("Hello\n")
+    df = pd.DataFrame([input_data['answers']], columns=column_names)
+    # df = map_ratings(df, 'GenHealth')
+    # df = map_ratings(df, 'OvrDietHealth')
+    print('New DF\n')
+    print(df.head())
+    # You can add more preprocessing steps here if needed
+    return df
 
 @app.route('/api/submit_answers', methods=['POST'])
 def submit_answers():
-    data = request.get_json(force=True)
-    print(data)  # For debugging purposes
-    return jsonify({'status': 'success', 'data': data})
-
-# @app.route('/api/get_answers', methods=['GET'])
-# def get_answers():
-#     return jsonify({'answers': answers_storage})
+    try:
+        data = request.get_json(force=True)
+        df = preprocess_data(data)  # Convert received data to DataFrame
+        #print(df)  # For debugging purposes, prints DataFrame to console
+        return jsonify({'status': 'success', 'data': data})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
